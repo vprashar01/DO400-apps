5d8e8b448727804e9977da8bdf604a1ad01a8b8c
Student User <student@workstation.lab.example.com>
2025-04-04 16:22:47 -0400

    Refactor1

--- simple-webapp/backend/server.js
+++ simple-webapp/backend/server.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.end(greet(name));
+    res.end(greeting);
 });
 
 const port = 3000;
